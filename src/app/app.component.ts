70bfb4d1dc6d1558215420d82a220dbf41c3e39c
Ronan Lina <ronanlina@gmail.com>
2018-04-19 14:53:37 +0800

    angular JS seminar. Pipe lecture follow through

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public myId: string = "myAngularId";
+  public myValue: string = "inputVal";
+  //public isSuccess: boolean = true;
+  public colorGreen: string = "orange";
+  public alert: string = "";
+  public show: boolean = true;
+  public check: string = "gotcha";
+  //public students: any = [];
+
+  public inputValue: string= "uniqueId";
+  public isSuccess: boolean = false;
+  public studentList: any = [];
+
+  public dateObject = new Observable<string>((subs: Subscriber<string>)=>{
+      setInterval(()=>{subs.next(new Date().toString() );}, 1000);
+  });
+
+  public keyword: String;
+  public login = {
+      success: this.isSuccess,
+      error: false
+  };
+
+  public student = {
+    id : '',
+    fname : '',
+    mname : '',
+    lname : ''
+  };
+  
+  public notification = {
+    class : '',
+    message : ''
+  };
+
+  continueAdd(){
+    return (this.student.fname.length > 0 && this.student.mname.length > 0 && this.student.lname.length > 0);
+  }
+
+  addStudent(){
+    let newData: any;
+    newData = Object.assign({}, this.student);
+
+    this.studentList.push( newData );
+
+    for(let stud in this.student){
+      this.student[stud] = '';
+    }
+  }
+
+  public edit: boolean = false;
+  getStudent(data){
+    this.student = data;
+
+    this.edit = true;
+  }
+
+  valueChanged(update){
+    console.log(update);
+  }
+
+  clear(){
+
+    let copy = Object.assign({}, this.student);
+
+    for(let copyData in copy){
+      copy[copyData] = '';
+    }
+    this.student = copy;
+  }
+
+  newStudent(){
+    this.edit = false;
+  }
+
+  isValid(input){
+    if(input.length > 0){
+      this.notification.class = 'text-success';
+      this.notification.message = 'valid first name';
+    }
+    else{
+      this.notification.class = 'text-danger';
+      this.notification.class = 'invalid first name!';
+    }
+  }
+  
+  alertNow(input){
+    this.alert = input.value;
+  }
+
+  /*addStudent(student){
+    this.students.push(student.value);
+  }*/
+
+  removeEntry(val: number){
+    this.studentList.splice(val,1);  
+  }
+
 }
---
+++ src/app/search-pipe.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchPipe'
+})
+export class SearchPipePipe implements PipeTransform {
+
+  transform(list: any[], keyword: string): any {
+    if(!list){
+      return [];
+    }
+    if(!keyword){
+      return list;
+    }
+
+    keyword = keyword.toLowerCase();
+
+    return list.filter(index=>{
+      if(index.fname.toLowerCase().includes( keyword )){
+        return true;
+      }
+    });
+    
+  }
+
+}
